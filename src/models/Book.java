5cd247a8a9a55814bb32c54eae9c6f330d33b86d
jrojas10 <jrojasbf@gmail.com>
2018-03-21 10:49:25 -0700

    created initial classes

--- src/models/Book.java
+++ src/models/Book.java
@@ -1,5 +1,58 @@
 package models;
 
 public class Book {
+	int count = 0;
+	int id;
+	String name;
+	String author;
+	int isbn;
+	String category;
+	
+public Book(String name, String author, int isbn, String category) {
+	this.id = count++;
+	this.name = name;
+	this.author = author;
+	this.isbn= isbn;
+	this.category = category;
+}
 
+public int getCount() {
+	return count;
+}
+
+public void setCount(int count) {
+	this.count = count;
+}
+
+public String getName() {
+	return name;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+
+public String getAuthor() {
+	return author;
+}
+
+public void setAuthor(String author) {
+	this.author = author;
+}
+
+public int getIsbn() {
+	return isbn;
+}
+
+public void setIsbn(int isbn) {
+	this.isbn = isbn;
+}
+
+public String getCategory() {
+	return category;
+}
+
+public void setCategory(String category) {
+	this.category = category;
+}
 }
---
+++ src/models/Student.java
@@ -0,0 +1,66 @@
+package models;
+
+public class Student {
+	int count = 0;
+	String firstName;
+	String lastName;
+	String email;
+	String password;
+	int id;
+
+	public Student(String firstName, String lastName, String email, String password) {
+		this.id = count++;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.password = password;
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+}
