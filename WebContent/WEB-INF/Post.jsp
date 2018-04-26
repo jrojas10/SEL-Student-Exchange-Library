7c6e167ffd3d1d399ffadbdea86f342b49bed08b
jesus-r-mendoza <jesus-r-mendoza@yahoo.com>
2018-04-26 08:57:36 -0700

    wrote Post.jsp and AddBook.java, couldn't get it to post to database yet

--- WebContent/WEB-INF/Post.jsp
+++ WebContent/WEB-INF/Post.jsp
@@ -13,46 +13,46 @@
 
       <h1>Post your book on SEL</h1>
 
-      <form action="" method="get">
+      <form action="AddBook" method="get">
         
         <div class = "item">
           <label>Title</label>
-          <input type="text" placeholder = "Ex. Analysis of Algothims">
+          <input type="text" name = "title" placeholder = "Ex. Analysis of Algothims">
         </div>
         
         <div class = "item">
           <label>Author's First Name</label>
-          <input type="text" placeholder = "Ex. Micheal">
+          <input type="text" name = "authFirstName" placeholder = "Ex. Micheal">
         </div>
         
         <div class = "item">
           <label>Author's Last Name</label>
-          <input type="text" placeholder = "Ex. Cohem">
+          <input type="text" name="authLastName" placeholder = "Ex. Cohem">
         </div>
         
         <div class = "item">
           <label>ISBN# </label>
-          <input type="text" placeholder = "Ex. 123-1-1234-1234-1">
+          <input type="text" name="isbn" placeholder = "Ex. 123-1-1234-1234-1">
         </div>
         
         <div class = "item">
           <label>Subject </label>
-          <input type="text" placeholder = "Ex. Computer Science">
+          <input type="text" name="subject" placeholder = "Ex. Computer Science">
         </div>
         
         <div class = "item">
           <label>Class </label>
-          <input type="text" placeholder = "Ex. CS-3112">
+          <input type="text" name="ofClass" placeholder = "Ex. CS-3112">
         </div>
         
         <div class = "item">
           <div class="rad_btns">
             <label>Condition</label>
 
-            <div class = rad> <input type="radio" name="condition">New </div>
-            <div class = rad> <input type="radio" name="condition">Good </div>
-            <div class = rad> <input type="radio" name="condition">Mid </div>
-            <div class = rad> <input type="radio" name="condition">Poor </div>
+            <div class = rad> <input type="radio" name="condition">New</div>
+            <div class = rad> <input type="radio" name="condition">Good</div>
+            <div class = rad> <input type="radio" name="condition">Mid</div>
+            <div class = rad> <input type="radio" name="condition">Poor</div>
 
           </div>
         </div>
@@ -61,7 +61,7 @@
           <label>Price </label> 
           <div class="price" style="display: flex;">
             <label> $ </label>
-            <input type="number" placeholder = "Ex. 12">
+            <input type="number" name="price" placeholder = "Ex. 22">
           </div>          
         </div>
         
--- src/mvc/AddBook.java
+++ src/mvc/AddBook.java
@@ -1,11 +1,18 @@
 package mvc;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import models.Config;
 
 /**
  * Servlet implementation class AddBook
@@ -19,7 +26,6 @@ public class AddBook extends HttpServlet {
      */
     public AddBook() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
 	/**
@@ -34,8 +40,74 @@ public class AddBook extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		
+		String[] inputs = new String[8];
+				
+		//get parameters
+		String title, aFirst, aLast, isbn, sub, clas, condition, price;
+		
+		title = request.getParameter("title");                  inputs[0] = title.trim();
+		aFirst = request.getParameter("authFirstname");         inputs[1] = aFirst.trim();
+		aLast = request.getParameter("authLastName");           inputs[2] = aLast.trim();
+		isbn = request.getParameter("isbn");                    inputs[3] = isbn.trim();
+		sub = request.getParameter("Subject");                  inputs[4] = sub.trim();
+		clas = request.getParameter("ofClass");                 inputs[5] = clas.trim();
+		condition = request.getParameter("condition");          inputs[6] = condition.trim();
+		price = request.getParameter("price");                  inputs[7] = price.trim();
+		
+		// check if one or more parameters are empty or null
+		if ( incomplete(inputs) ) {
+			response.sendRedirect("Post");
+			return;
+		}
+		
+		Connection c = null;
+		try {
+			Config cfg = new Config();
+			//String url = cfg.getProperty("dbUrl");
+			String username = cfg.getProperty("dbUserName");
+			String password = cfg.getProperty("dbPassword");
+			
+			
+			String url = "jdbc:mysql://cs3.calstatela.edu/cs3220stu49";
+			//String username = "cs3220stu49";
+			//String password = "#Enwva2#";
+			
+			
+			// writing database query
+			
+			String sql = "INSERT INTO `Books2` (`BookID`, `Price`, `Title`, `ISBN`, `AuthorFirst`, `AuthorLast`, `Subject`, `Class`, `State`) VALUES (0, ?, ?, ?, ?, ?, ?, ?, ?);";
+			c = DriverManager.getConnection(url, username, password);
+			PreparedStatement pstmt = c.prepareStatement(sql);
+			pstmt.setString(1, title);
+			pstmt.setString(2, aFirst);
+			pstmt.setString(3, aLast);
+			pstmt.setString(4, isbn);
+			pstmt.setString(5, sub);
+			pstmt.setString(6, clas);
+			pstmt.setString(7, condition);
+			pstmt.setString(8, price);
+			pstmt.executeUpdate();
+			
+		} catch (SQLException e) {
+			throw new ServletException(e);
+		} finally {
+			try {
+				if (c != null)
+					c.close();
+			} catch (SQLException e) {
+				throw new ServletException(e);
+			}
+		}
+		response.sendRedirect("Profile");	
+		
 	}
 
+	private boolean incomplete(String[] strs) {
+		for (String s : strs) {
+			if ( s.length() == 0 || s == null )
+				return true;
+		}
+		return false;
+	}
 }
